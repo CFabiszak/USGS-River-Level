e232b1ac7121d591bde66baa9fda1f6c1eb096a4
CFabiszak <codyfab@comcast.net>
2017-08-20 19:15:12 -0400

    Add files via upload

---
+++ USGS.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+path=r'C:\Users\codyf\Desktop\driver\chromedriver'
+import time
+from selenium.webdriver.common.keys import Keys
+
+option= webdriver.ChromeOptions()
+option.add_argument('--incognito')
+
+driver=webdriver.Chrome(path,chrome_options=option)
+driver.get("https://waterdata.usgs.gov/usa/nwis/uv?01636500")
+
+search= driver.find_element_by_xpath("""//*[@id="html"]""").click()
+search_day=driver.find_element_by_xpath("""//*[@id="begin_date"]""").click()
+search_day_clear=driver.find_element_by_xpath("""//*[@id="begin_date"]""").clear()
+date=search_day_clear=driver.find_element_by_xpath("""//*[@id="begin_date"]""")
+date.send_keys((time.strftime("%Y-%m-%d")))
+date.send_keys(Keys.RETURN)
+date.send_keys(Keys.RETURN)
+post = driver.find_elements_by_xpath("""/html/body/table[1]/tbody""")
+
+for posts in post:
+    print(posts.text )
+driver.quit()
